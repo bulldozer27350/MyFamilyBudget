8e50210139a5a2fdfcbfa9e94cc927243537ebd0
Marc Olivier ETOURNEAU <mo.etourneau@gmail.com>
2026-09-24 05:54:50 +0200

    [FIX] Apply patch: 0014-enable-banking-java.patch via AutoPatcher

--- back/server/src/main/java/com/moe/myfamilybudget/server/internal/enablebanking/EnableBankingConfig.java
+++ back/server/src/main/java/com/moe/myfamilybudget/server/internal/enablebanking/EnableBankingConfig.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -41,6 +42,7 @@ public class EnableBankingConfig {
     private String unavailableReason;
     private EnableBankingJwtSigner signer;
 
+    @Autowired
     public EnableBankingConfig(
             @Value("${myfamilybudget.enable-banking.application-id:}") String applicationId,
             @Value("${myfamilybudget.enable-banking.private-key-path:}") String privateKeyPath,
